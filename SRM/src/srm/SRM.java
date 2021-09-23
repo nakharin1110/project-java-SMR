103fd1c6fdde0ba7ada307d7ce8f8b660f8c0090
nakharin1110 <68723114+nakharin1110@users.noreply.github.com>
2021-09-23 16:55:57 +0700

    last

---
+++ SRM/src/srm/SRM.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package srm;
+
+/**
+ *
+ * @author NAKHARIN
+ */
+public class SRM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
